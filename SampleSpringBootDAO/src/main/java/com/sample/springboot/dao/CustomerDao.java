1f3dc71982240b88fffb4f80b8ebeabc032f2fe5
takuyaTT <you@example.com>
2020-08-08 16:43:44 +0900

    delete SampleBootDao

--- SampleSpringBootDAO/src/main/java/com/sample/springboot/dao/CustomerDao.java
+++
@@ -1,8 +0,0 @@
-package com.sample.springboot.dao;
-
-import java.io.Serializable;
-import java.util.List;
-
-public interface CustomerDao <T> extends Serializable {
-    public List<T> getAllCustomer();
-}
